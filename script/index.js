e8eca6dca3a0951ed47a430a6fd92d2cef5f71a8
bromodragonfly <87935674+bromodragonfly@users.noreply.github.com>
2023-02-17 19:27:23 +0300

    stack 1V

--- script/index.js
+++ script/index.js
@@ -140,3 +140,20 @@ gsap.from('.about__text', {
     opacity: 0,
     duration: 1.2,
 })
+
+const handleMouseMove = (e) => {
+    const { currentTarget: target } = e
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top
+
+    target.style.setProperty('--mouse-x', `${x}px`)
+    target.style.setProperty('--mouse-y', `${y}px`)
+}
+
+const cardsList = document.querySelectorAll('.project__item')
+
+cardsList.forEach((item) => {
+    item.addEventListener('mousemove', (e) => handleMouseMove(e))
+})
